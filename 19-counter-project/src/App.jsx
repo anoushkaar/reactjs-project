01b602b03efd90f464450ca9583f82e0c51f7d40
Anoushka Raghuveeran <anoushkaraghuveeran@gmail.com>
2025-12-07 00:03:30 +0530

    feat: implement Zustand for state management in counter app

--- 19-counter-project/src/App.jsx
+++ 19-counter-project/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+// import { useState } from "react";
+import useCounter from './store/counterStore';
 
 const App = () => {
-  const [value, setValue] = useState(0);
+  // const [value, setValue] = useState(0);
 
-  function handleBtn(val) {
-    setValue(val);
-  }
+  // function handleBtn(val) {
+  //   setValue(val);
+  // }
+  const count = useCounter((state)=>(state.count))
+  const increase = useCounter((state)=>(state.increase))
+  const decrease = useCounter((state)=>(state.decrease))
+  const reset = useCounter((state)=>(state.reset))
+
   const anous = {
     backgroundColor: "blue",
     padding: "2px",
@@ -26,12 +32,18 @@ const App = () => {
       }}
     >
       <h1>Counter app</h1>
-      <h2>{value}</h2>
+      {/* <h2>{value}</h2> */}
+      <h2>{count}</h2>
       <div style={anous}>
-        <button onClick={() => handleBtn(value + 1)}>Add</button>
-        <button onClick={() => (value > 0 ? handleBtn(value - 1) : 0)}>
+        <button onClick={increase}>Add</button>
+        <button onClick={decrease}>
           Diff
         </button>
+        <button onClick={reset}>reset</button>
+        {/* <button onClick={() => handleBtn(value + 1)}>Add</button>
+        <button onClick={() => (value > 0 ? handleBtn(value - 1) : 0)}>
+          Diff
+        </button> */}
       </div>
     </div>
   );
